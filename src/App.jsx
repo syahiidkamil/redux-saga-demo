33ca13f1894ca6444416bf170c7fa82b2b96c6c4
Syahiid Nur Kamil <syahiidkamil.tech@gmail.com>
2024-04-04 12:00:28 +0700

    add jsconfig.json and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import { Button } from "@nextui-org/react";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 
-function App() {
+function Home() {
   return (
     <div>
       <Button color="primary">Press me</Button>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
